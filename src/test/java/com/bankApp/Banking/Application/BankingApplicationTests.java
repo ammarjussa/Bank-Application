d513156c35c47adce91513129d1f788619c50e98
Ammar Jussa <ammar26497@gmail.com>
2022-12-27 19:38:16 +0000

    first commit

---
+++ src/test/java/com/bankApp/Banking/Application/BankingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankApp.Banking.Application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
